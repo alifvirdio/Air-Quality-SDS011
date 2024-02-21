59cd8266d681911a4b1f170d7cb41ec710f1da72
alifvirdio <alifvirdio990@gmail.com>
2024-02-21 09:18:03 +0700

    nambah dikit

--- sds011.ino
+++ sds011.ino
@@ -2,6 +2,8 @@
 #include <SDS011.h>
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
+#include <WebServer.h>
+// #include <ESPAsyncWebServer.h>
 
 int coloumnsLcd = 20;
 int rowsLcd = 4;
@@ -13,66 +15,72 @@ const char* password = "hiluxpertek";
 //init objek
 SDS011 sds;
 LiquidCrystal_I2C lcd(0x27, coloumnsLcd, rowsLcd);
+WebServer server(80);
+
+void wifi_connect_flag() {
+  lcd.setCursor(0, 0);  //pojok kiri atas
+  lcd.print("Connecting..");
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("Tidak Terhubung \n");
+  }
+
+  // Serial.println("");
+  // Serial.println("WiFi connected.");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
+
+  lcd.setCursor(0, 1);
+  lcd.print("Connected!");
+  lcd.setCursor(0, 2);
+  lcd.print("SSID : ");
+  lcd.print(ssid);
+  lcd.setCursor(0, 3);
+  lcd.print(WiFi.localIP());
+  delay(5000);
+  lcd.clear();
+}
 
 void setup() {
-    Serial.begin(9600);
-    sds.begin(16, 17);  //GPIO16 (RX) dan GPIO17 (TX)
-    lcd.init();
-    lcd.backlight();
+  Serial.begin(9600);
+  sds.begin(16, 17);  //GPIO16 (RX) dan GPIO17 (TX)
+  lcd.init();
+  lcd.backlight();
 
-    lcd.setCursor(0,0); //pojok kiri atas
-    lcd.print("Connecting..");
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print("Tidak Terhubung \n");
-    }
-    // Print local IP address and start web server
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    lcd.setCursor(0, 2);
-    lcd.print("Connected!");
-    lcd.setCursor(0, 3);
-    lcd.print(WiFi.localIP());
-    delay(5000);
-    lcd.clear();
+  wifi_connect_flag();
 }
 
 void loop() {
 
-    lcd.setCursor(0,0); //pojok kiri atas
-    lcd.print("Air Quality Index!!");
-    lcd.setCursor(0,1);
-    lcd.print("BMKG JUANDA x PENS");
+  lcd.setCursor(0, 0);  //pojok kiri atas
+  lcd.print("Air Quality Index!!");
+  lcd.setCursor(0, 1);
+  lcd.print("BMKG JUANDA x PENS");
 
-    float pm25, pm10;
-    int error = sds.read(&pm25, &pm10);
+  float pm25, pm10;
+  int error = sds.read(&pm25, &pm10);
 
-    if (!error) {
-        // Serial.print("PM2.5:");
-        // Serial.print(pm25);
-        // Serial.print(" µg/m³ ");
-        // Serial.print("PM10 :");
-        // Serial.print(pm10);
-        // Serial.println(" µg/m³");
-        //lcd
-        lcd.setCursor(0,2);
-        lcd.print("PM 2.5: ");
-        lcd.print(pm25);
-        
-        lcd.setCursor(0,3);
-        lcd.print("PM 10 : ");
-        lcd.print(pm10);
-    } else {
-        Serial.println("Failed to read from SDS011 sensor.");
-    }
+  if (!error) {
+    // Serial.print("PM2.5:");
+    // Serial.print(pm25);
+    // Serial.print(" µg/m³ ");
+    // Serial.print("PM10 :");
+    // Serial.print(pm10);
+    // Serial.println(" µg/m³");
+    //lcd
+    lcd.setCursor(0, 2);
+    lcd.print("PM 2.5: ");
+    lcd.print(pm25);
 
-    // Tunggu sebentar sebelum membaca kembali
-    delay(1000); // Delay 1 detik (1000 milidetik)
+    lcd.setCursor(0, 3);
+    lcd.print("PM 10 : ");
+    lcd.print(pm10);
+  } else {
+    Serial.println("Failed to read from SDS011 sensor.");
+  }
+  delay(1000);  // Delay 1 detik (1000 milidetik)
 }
